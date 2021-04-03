abb7ee21d499489198a9b9df4419f12eba3ade8b
Xiaoqiang-Yan <55470388+Xiaoqiang-Yan@users.noreply.github.com>
2021-04-03 11:40:16 +0800

    Delete run_eval.py

--- run_eval.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.system("python eval.py --config cfgs/Cifar-100/t1.yaml && "
-          "python eval.py --config cfgs/Cifar-100/t2.yaml && "
-          "python eval.py --config cfgs/Cifar-100/t3.yaml")
