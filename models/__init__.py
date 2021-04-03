6e06740e6e3fb92d4490933e962aa1d82fad7eaa
Xiaoqiang-Yan <55470388+Xiaoqiang-Yan@users.noreply.github.com>
2021-04-03 11:38:16 +0800

    Delete models directory

--- models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .classifier import *
-from .dim_Model import *
-from .alexnet import *
-from .alexnet_p import *
-from .dim_Model_og import *
-
--- models/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .vgg16 import *
-from .alexnet import *
